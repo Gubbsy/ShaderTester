9b58e05f99d5091fe6c00c2f86ae0354358b4d87
Adam Lee <adam.lee@students.plymouth.ac.uk>
2019-12-29 22:51:58 +0000

    Fix culling

--- ShaderTester/Main.cpp
+++ ShaderTester/Main.cpp
@@ -32,9 +32,9 @@ Model* model = nullptr;
 void
 init(void)
 {
-	glFrontFace(GL_CW);
-	glCullFace(GL_BACK);
 	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
 	glEnable(GL_DEPTH_TEST);
 }
 
